7585094dc70ba43c20e56f05cce0a52b8d31a4db
Afzal <afzal23925254@gmail.com>
2025-03-20 15:36:31 +0530

    Calculator project

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("HI This file i have created to do some practice")
+print("This is a calculator program ")
+a=int(input("Enter your first number : "))
+b=int(input("Enter your second number:"))
+
+def add(a,b):
+    print(a+b)
+
+def mul(a,b):
+    print(a*b)
+
+inp=input("Which opertaion you want to perform : ")
+
+if inp=="add":
+    add(a,b)
+else:
+    mul(a,b)
